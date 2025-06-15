2b4ec8dcd11ca6e16c89b4e7fad646727262d4d6
J-Goodwin247 <jordan.goodwin@csuglobal.edu>
2025-06-15 06:09:31 -0600

    Add files via upload

---
+++ PersonInfoCriticalThinking1.java
@@ -0,0 +1,18 @@
+public class PersonInfoCriticalThinking1 {
+    public static void main(String[] args) {
+        // PersonInfo Variables
+        String firstName = "Jordan";
+        String lastName = "Goodwin";
+        String streetAddress = "123 Fake St";
+        String city = "Nowhere";
+        String zipCode = "54321";
+
+        // Print information
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Street Address: " + streetAddress);
+        System.out.println("City: " + city);
+        System.out.println("Zip Code: " + zipCode);
+    }
+
+} 
