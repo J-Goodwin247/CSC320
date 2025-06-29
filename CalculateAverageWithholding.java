8142c78b0b175ed50f5aef290db8dab9b6d8900c
J-Goodwin247 <jordan.goodwin@csuglobal.edu>
2025-06-29 06:34:35 -0600

    Add files via upload
    Module 3: Calculating Average Withholding with if-else-if statments

---
+++ CalculateAverageWithholding.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class CalculateAverageWithholding {
+
+    public static void main(String [] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Ask user for weekly income
+        System.out.print("Enter Weekly Income $");
+        double weeklyIncome = scanner.nextDouble();
+        double taxRate;
+
+        //Determine the user tax rate based on their income
+        if (weeklyIncome < 500){
+            taxRate = 0.10;
+        }else if (weeklyIncome >=500 && weeklyIncome < 1500){
+            taxRate = 0.15;
+        }else if (weeklyIncome >= 1500 && weeklyIncome < 2500){
+            taxRate = 0.20;
+        }else {
+            taxRate = 0.30;
+        }
+
+        //calculation of tax withholding
+        double taxWithholding = weeklyIncome * taxRate;
+
+        System.out.printf("Weekly Tax Withholding: $%.2f%n", taxWithholding);
+
+        scanner.close();
+    }  
+}
