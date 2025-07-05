29fc3c3a29ad82dbda46f9c514559e34d674e920
J-Goodwin247 <jordan.goodwin@csuglobal.edu>
2025-07-05 16:38:11 -0600

    Update GradeStatistics.java

--- GradeStatistics.java
+++ GradeStatistics.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
 public class GradeStatistics {
+    //declare variables for float
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         float sum = 0;
         float max = Float.MIN_VALUE;
         float min = Float.MAX_VALUE;
-
+        //defining how the user enters grades within a for loop.
         System.out.println("Enter 10 grades:");
 
         for (int i = 0; i < 10; i++) {
@@ -15,6 +16,7 @@ public class GradeStatistics {
                 System.out.println("Please enter a valid floating-point number.");
                 scanner.next(); // clear the invalid input
             }
+            //defining of max, min and average 
             float grade = scanner.nextFloat();
             sum += grade;
             max = Math.max(max, grade);
@@ -22,7 +24,7 @@ public class GradeStatistics {
         }
 
         float average = sum / 10;
-
+        //printing results and closing to program
         System.out.println("Average Grade: " + average);
         System.out.println("Maximum Grade: " + max);
         System.out.println("Minimum Grade: " + min);
