e41ea84baef5de932aaea0f7d541a964cbdca728
J-Goodwin247 <jordan.goodwin@csuglobal.edu>
2025-07-18 09:59:55 -0600

    Update Automobile.java

--- Automobile.java
+++ Automobile.java
@@ -29,3 +29,20 @@ public class Automobile {
       System.out.println("Error in parameterized contructor: " + e.getMessage());
     }
   }
+
+  public String addNewVehicle(String make, String model, String color, int year, int mileage) {
+    try {
+      this.make = make;
+      this.model = model;
+      this.color = color;
+      this.year = year;
+      this.mileage = mileage;
+      return "Vehicle added successfully.";
+    } catch (Exception e) {
+      return "Error: Could not add vehicle." +e.getMessage();
+    }
+  }
+
+   public String[] info = listVehicleInformation() {
+     try{
+      String[] info = new String[5]
